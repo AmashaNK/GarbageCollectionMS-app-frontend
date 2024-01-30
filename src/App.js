88f47d036c75328718fcf2d4a0f5d37e976ba094
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-01-30 23:18:43 +0530

    code is formatted

--- src/App.js
+++ src/App.js
@@ -12,8 +12,25 @@ import SideNavbar from './Components/SideNavbar'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import Dashboard from './Components/MainPages/AdminDashboard/Dashboard'
+import Collection_Route from './Components/Pages/CollectionRoutePage/Collection_Route'
 
 function App() {
+    return (
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/news" element={<NewsPage />} />
+            <Route path="/contact-us" element={<ContactUs />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignupForm />} />
+            <Route path="/admin-dashboard" element={<Dashboard />} />
+            <Route path="/route" element={<Collection_Route />} />
+          </Routes>
+        </div>
+      </Router>
+    );
   return (
     <Router>
       <div>
