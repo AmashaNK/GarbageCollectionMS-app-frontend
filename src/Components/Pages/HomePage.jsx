94d9d62ef046dd890ba873bae01eef7956cf6c8a
Madhara Chathurangi <142675042+madhara1020@users.noreply.github.com>
2024-01-31 07:06:31 +0530

    create homepage with slider

--- src/Components/Pages/HomePage.jsx
+++ src/Components/Pages/HomePage.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Banner1 from "../Assets/banner.jpg";
+import Banner2 from "../Assets/banner2.jpg";
+import Banner3 from "../Assets/banner3.jpg";
+import "./HomePage.css";
 
-class HomePage extends React.Component {
-    render(){
-        return (
-            <h1></h1>
-          )
-    }
-  
-}
+import { Paper } from "@mui/material";
+
+// ... (imports)
+
+const HomePage = () => {
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
+
+  return (
+    <div>
+      <div className="headerContainer">
+        <h1>Smart Collector</h1>
+        <p>Think before your trash</p>
+        <Link to="/signup">
+          <button>Sign Up</button>
+        </Link>
+      </div>
+      <Slider {...sliderSettings}>
+        <div
+          className="slider-item"
+          style={{ backgroundImage: `url(${Banner1})` }}
+        ></div>
+        <div
+          className="slider-item"
+          style={{ backgroundImage: `url(${Banner2})` }}
+        ></div>
+        <div
+          className="slider-item"
+          style={{ backgroundImage: `url(${Banner3})` }}
+        ></div>
+      </Slider>
+    </div>
+  );
+};
 
 export default HomePage;
