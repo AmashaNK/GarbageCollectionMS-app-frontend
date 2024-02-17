5c68638693ded8ed2c513368f32af7041e060a50
sawan98 <lilankasawan@gmail.com>
2024-02-17 22:37:31 +0530

    Change sidenavbar

---
+++ src/Components/Assets/AdminMenuList.jsx
@@ -0,0 +1,199 @@
+import { Menu } from 'antd'
+import { HomeOutlined, BarsOutlined, AppstoreOutlined, AccountBookFilled, PayCircleOutlined, SettingOutlined  } from '@ant-design/icons';
+
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+const drawerWidth = 200;
+
+function ResponsiveDrawer(props) {
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const [isClosing, setIsClosing] = React.useState(false);
+
+    const handleDrawerClose = () => {
+        setIsClosing(true);
+        setMobileOpen(false);
+    };
+
+    const handleDrawerTransitionEnd = () => {
+        setIsClosing(false);
+    };
+
+    const handleDrawerToggle = () => {
+        if (!isClosing) {
+            setMobileOpen(!mobileOpen);
+        }
+    };
+
+    const drawer = (
+        <div>
+            <Toolbar />
+            
+            <List sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '80%', marginTop: '10px', marginLeft: '2px' }}>
+                {[
+                    { text: 'Inbox', icon: <InboxIcon /> },
+                    { text: 'Starred', icon: <MailIcon /> },
+                    { text: 'Send email', icon: <InboxIcon /> },
+                    { text: 'Drafts', icon: <MailIcon /> },
+                    { text: 'All mail', icon: <InboxIcon /> },
+                    { text: 'Trash', icon: <MailIcon /> },
+                    { text: 'Spam', icon: <InboxIcon /> }
+                ].map((item, index) => (
+                    <ListItem key={item.text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {item.icon}
+                            </ListItemIcon>
+                            <ListItemText primary={item.text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+    // Remove this const when copying and pasting into your project.
+    const container = window !== undefined ? () => window().document.body : undefined;
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                sx={{
+                    width: { sm: `calc(100% - ${drawerWidth}px)` },
+                    ml: { sm: `${drawerWidth}px` },
+                }}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{ mr: 2, display: { sm: 'none' } }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        Responsive drawer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Box
+                component="nav"
+                sx={{ width: { xs: '100%', sm: drawerWidth }, flexShrink: { sm: 0 } }}
+                aria-label="mailbox folders"
+            >
+                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                <Drawer
+                    container={container}
+                    variant="temporary"
+                    open={mobileOpen}
+                    onTransitionEnd={handleDrawerTransitionEnd}
+                    onClose={handleDrawerClose}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: { xs: 'block', sm: 'none' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+                <Drawer
+                    variant="permanent"
+                    sx={{
+                        display: { xs: 'none', sm: 'block' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                    open
+                >
+                    {drawer}
+                </Drawer>
+            </Box>
+            <Box
+                component="main"
+                sx={{ flexGrow: 1, p: 3, width: { xs: '100%', sm: `calc(100% - ${drawerWidth}px)` }, textAlign:'center' }}
+            >
+                <Toolbar />
+                    <Typography paragraph sx={{ textAlign: 'justify',paddingLeft: '210px' }}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+                        enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+                        imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+                        Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+                        Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                        adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+                        nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+                        leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+                        feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+                        consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+                        sapien faucibus et molestie ac.
+                    </Typography>
+                
+                
+            </Box>
+        </Box>
+    );
+}
+
+ResponsiveDrawer.propTypes = {
+    /**
+     * Injected by the documentation to work in an iframe.
+     * Remove this when copying and pasting into your project.
+     */
+    window: PropTypes.func,
+};
+
+export default ResponsiveDrawer;
+
+
+/*const AdminMenuList = () => {
+
+    return (
+
+        <Menu theme="dark" mode="inline">
+
+            <Menu.Item key="home" icon={<HomeOutlined />}>
+                View All Bin Data
+            </Menu.Item>
+            <Menu.Item key="activity" icon={<HomeOutlined />}>
+                View Filled Bin Data
+            </Menu.Item>
+            <Menu.SubMenu key="tasks" icon={<BarsOutlined />} title="tasks">
+                <Menu.Item key="task-1">Task1</Menu.Item>
+                <Menu.Item key="task-2">Task2</Menu.Item>
+            </Menu.SubMenu>
+            <Menu.Item key="Add Truck Driver" icon={<AccountBookFilled />}>
+                Add Truck Driver
+            </Menu.Item>
+            <Menu.Item key="Add Truck" icon={<PayCircleOutlined />}>
+                Add Truck
+            </Menu.Item>
+            <Menu.Item key="Available Drives" icon={<SettingOutlined />}>
+                Available Drivers
+            </Menu.Item>
+
+        </Menu>
+    )
+};
+
+export default AdminMenuList;*/
