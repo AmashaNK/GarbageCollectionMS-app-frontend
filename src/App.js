27574607624594ee0a201d1a3aae1774322cc733
AmashaNK <142134191+AmashaNK@users.noreply.github.com>
2024-02-18 22:58:07 +0530

    Small changes in admin dashboard

--- src/App.js
+++ src/App.js
@@ -6,30 +6,44 @@ import HomePage from './Components/Pages/HomePage/HomePage'
 import NewsPage from './Components/Pages/NewsPage/NewsPage'
 import AboutUs from './Components/Pages/AboutUsPage/AboutUs'
 import ContactUs from './Components/Pages/ContactUsPage/ContactUs'
-import Sidebar from './Components/Sidebar'
-import Navbar from './Components/Navbar'
-import SideNavbar from './Components/SideNavbar'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
-import Dashboard from './Components/MainPages/AdminDashboard/Dashboard'
+import Dashboard from './Components/MainPages/Admin/Dashboard'
 import Collection_Route from './Components/Pages/CollectionRoutePage/Collection_Route'
 
 function App() {
-    return (
-      <Router>
-        <div>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/news" element={<NewsPage />} />
-            <Route path="/contact-us" element={<ContactUs />} />
-            <Route path="/about-us" element={<AboutUs />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignupForm />} />
-            <Route path="/admin-dashboard" element={<Dashboard />} />
-            <Route path="/route" element={<Collection_Route />} />
-          </Routes>
-        </div>
-      </Router>
+    return ( <
+        Router >
+        <
+        div >
+        <
+        Routes >
+        <
+        Route path = "/"
+        element = { < HomePage / > }
+        /> <
+        Route path = "/news"
+        element = { < NewsPage / > }
+        /> <
+        Route path = "/contact-us"
+        element = { < ContactUs / > }
+        /> <
+        Route path = "/about-us"
+        element = { < AboutUs / > }
+        /> <
+        Route path = "/login"
+        element = { < Login / > }
+        /> <
+        Route path = "/signup"
+        element = { < SignupForm / > }
+        /> <
+        Route path = "/admin-dashboard"
+        element = { < Dashboard / > }
+        /> <
+        Route path = "/route"
+        element = { < Collection_Route / > }
+        /> <
+        /Routes> <
+        /div> <
+        /Router>
     );
 }
 
