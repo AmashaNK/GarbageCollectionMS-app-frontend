82062bc2452e5ce445e36774b0ed9e2a1b8ca88c
sawan98 <lilankasawan@gmail.com>
2024-02-18 23:34:19 +0530

    Corrected the routes

--- src/App.js
+++ src/App.js
@@ -10,40 +10,21 @@ import Dashboard from './Components/MainPages/Admin/Dashboard'
 import Collection_Route from './Components/Pages/CollectionRoutePage/Collection_Route'
 
 function App() {
-    return ( <
-        Router >
-        <
-        div >
-        <
-        Routes >
-        <
-        Route path = "/"
-        element = { < HomePage / > }
-        /> <
-        Route path = "/news"
-        element = { < NewsPage / > }
-        /> <
-        Route path = "/contact-us"
-        element = { < ContactUs / > }
-        /> <
-        Route path = "/about-us"
-        element = { < AboutUs / > }
-        /> <
-        Route path = "/login"
-        element = { < Login / > }
-        /> <
-        Route path = "/signup"
-        element = { < SignupForm / > }
-        /> <
-        Route path = "/admin-dashboard"
-        element = { < Dashboard / > }
-        /> <
-        Route path = "/route"
-        element = { < Collection_Route / > }
-        /> <
-        /Routes> <
-        /div> <
-        /Router>
+    return (
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/news" element={<NewsPage />} />
+            <Route path="/contact-us" element={<ContactUs />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignupForm />} />
+            <Route path="/admin-dashboard" element={<Dashboard />} />
+            <Route path="/route" element={<Collection_Route />} />
+          </Routes>
+        </div>
+      </Router>
     );
 }
 
