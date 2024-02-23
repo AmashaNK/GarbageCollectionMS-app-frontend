79b727e4a5ffb58823b58ac81b11cc1e540fc615
AmashaNK <142134191+AmashaNK@users.noreply.github.com>
2024-02-23 10:30:02 +0530

    Create truck driver dashboard

---
+++ src/Components/MainPages/TruckDriver/TruckDriverDashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import TruckDriverTaskList from './TruckDriverTaskList'
+
+const Dashboard = () => {
+  return (
+    <Box sx={{}}>
+      <TruckDriverTaskList /> 
+        <div>
+
+        </div>
+    </Box>
+  )
+}
+
+export default Dashboard
