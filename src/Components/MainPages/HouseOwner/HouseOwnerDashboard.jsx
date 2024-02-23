5a99513c7937be8bc5d8b9706559eef1b2f31322
AmashaNK <142134191+AmashaNK@users.noreply.github.com>
2024-02-23 10:29:10 +0530

    Create house owner dashboard

---
+++ src/Components/MainPages/HouseOwner/HouseOwnerDashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import HouseOwnerTaskList from './HouseOwnerTaskList'
+
+const Dashboard = () => {
+  return (
+    <Box sx={{}}>
+      <HouseOwnerTaskList /> 
+        <div>
+
+        </div>
+    </Box>
+  )
+}
+
+export default Dashboard
