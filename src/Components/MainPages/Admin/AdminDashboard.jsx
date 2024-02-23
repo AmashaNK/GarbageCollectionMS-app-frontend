ddb2b565d82485b588dbe227160926c39f03a3fc
AmashaNK <142134191+AmashaNK@users.noreply.github.com>
2024-02-23 10:24:17 +0530

    Create Admin Dashboard

---
+++ src/Components/MainPages/Admin/AdminDashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import AdminTaskList from './AdminTaskList'
+
+const Dashboard = () => {
+  return (
+    <Box sx={{}}>
+      <AdminTaskList /> 
+        <div>
+
+        </div>
+    </Box>
+  )
+}
+
+export default Dashboard
