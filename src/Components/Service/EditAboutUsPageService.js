298997ef6386115d3a0c3450d824d7a90a86fb74
AmashaNK <142134191+AmashaNK@users.noreply.github.com>
2024-02-29 11:01:47 +0530

    Service for edit about us page

---
+++ src/Components/Service/EditAboutUsPageService.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:8080/api/v1/auth';
+
+const editAboutUsPageData = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export const addAboutUsData = async(editData) => {
+    try {
+        const response = await editAboutUsPageData.post('/AboutUsPage/addAboutUsData', editData);
+        return response.data;
+    } catch (error) {
+        throw new Error('Error submitting data: ' + error.message);
+    }
+};
+
+export default editAboutUsPageData;
