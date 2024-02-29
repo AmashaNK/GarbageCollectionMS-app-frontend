ddda7154e21e7d2333da8fff93d0f98f56bcf267
AmashaNK <142134191+AmashaNK@users.noreply.github.com>
2024-02-29 11:02:52 +0530

    Service for add truck driver complaints

---
+++ src/Components/Service/TruckDriverComplaintService.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:8080/api/v1/auth';
+
+const complaintService = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export const submitComplaint = async(complaintData) => {
+    try {
+        const response = await complaintService.post('/TruckDriverComplaints/addComplaints', complaintData);
+        return response.data;
+    } catch (error) {
+        throw new Error('Error submitting complaint: ' + error.message);
+    }
+};
+
+export default complaintService;
