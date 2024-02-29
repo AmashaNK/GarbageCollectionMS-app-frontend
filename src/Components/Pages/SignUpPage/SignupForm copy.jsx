195874d3254bf5e2dabe2df408bab16ff6aee250
pubudu98coder <98485855+pubudu98coder@users.noreply.github.com>
2024-02-29 12:57:25 +0530

    some changes

--- src/Components/Pages/SignUpPage/SignupForm copy.jsx
+++ src/Components/Pages/SignUpPage/SignupForm copy.jsx
@@ -5,11 +5,10 @@ import { useState, useEffect ,useRef} from 'react';
 import { useNavigate } from 'react-router-dom';
 import Sidebar from '../../Sidebar';
 import Navbar from '../../Navbar';
-import axios from '../../api/axios'
 
 const USER_REGEX=/^[a-zA-Z][a-zA-Z0-9-_]{3,23}/;
 const PWD_REGX=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
-const REGISTER_URL='api/v1/auth/register'
+
 
 const SignupForm = () => {
     const userRef=useRef();
@@ -17,11 +16,9 @@ const SignupForm = () => {
 
     const navigate = useNavigate();
 
-    const [role, setRole] = useState('');
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [nicNo,setNicNo]=useState('');
+    const [selectedRole, setSelectedRole] = useState('');
+    const [name, setName] = useState('');
+    const [username, setUsername] = useState('');
     const [mobileNumber, setMobileNumber] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
@@ -44,7 +41,7 @@ const SignupForm = () => {
         //     password:'At least 12 characters long',
         //     confirmPassword: 'Re-enter your password'
         // },
-        HouseOwner: {
+        'HouseOwner': {
             name: 'Enter your full name',
             username: 'Use your e-mail as your username',
             mobileNumber:'Enter your mobile number',
@@ -53,44 +50,44 @@ const SignupForm = () => {
         }
     };
 
-    const [inputTitles, setInputTitles] = useState(roleInstructions[role] || {}); //Default to Admin instructions
+    const [inputTitles, setInputTitles] = useState(roleInstructions[selectedRole] || {}); //Default to Admin instructions
 
     useEffect(() => {
-        const newInputTitles = roleInstructions[role] || {};
+        const newInputTitles = roleInstructions[selectedRole] || {};
         console.log('New Input Titles:', newInputTitles);
         setInputTitles(newInputTitles);
-      }, [role]);
+      }, [selectedRole]);
     
 
     //Validate the signup form
 
     const validateForm = () => {
-        if(!role || !firstName || !lastName || !mobileNumber || !password || !confirmPassword){
+        if(!selectedRole || !name || !username || !mobileNumber || !password || !confirmPassword){
             alert('Please fill in all fields');
             return false;
         }
 
-        // switch(role){
-        //     case 'Admin':
-        //         if(!/^[A-Z]\d{4}$/.test(username)){
-        //             alert('Invalid type of username');
-        //             return false;
-        //         }
-        //         break;
-        //     // case 'Vehicle':
-        //     //     if(!/^[A-Z]{2,3}-\d{4}$/.test(username)){
-        //     //         alert('Invalid type of username');
-        //     //         return false;
-        //     //     }
-        //     //     break;
+        switch(selectedRole){
+            case 'Admin':
+                if(!/^[A-Z]\d{4}$/.test(username)){
+                    alert('Invalid type of username');
+                    return false;
+                }
+                break;
+            // case 'Vehicle':
+            //     if(!/^[A-Z]{2,3}-\d{4}$/.test(username)){
+            //         alert('Invalid type of username');
+            //         return false;
+            //     }
+            //     break;
 
-        //     case 'HouseOwner':
-        //         if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(username)){
-        //             alert('Enter your valid e-mail address.');
-        //             return false;
-        //         }
-        //         break;
-        // }
+            case 'HouseOwner':
+                if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(username)){
+                    alert('Enter your valid e-mail address.');
+                    return false;
+                }
+                break;
+        }
 
         //Password confirmation
 
@@ -102,29 +99,16 @@ const SignupForm = () => {
         return true;
     };
 
-    const handleSignup = async(e)=>{
-        e.preventDefault();
+    const handleSignup = () => {
         //Validation checks before making the signup request
-  //      if(validateForm()){
-            console.log('Selected Role:', role);
-            console.log('Firt Name:', firstName);
-            console.log('Username:', lastName);
+        if(validateForm()){
+            console.log('Selected Role:', selectedRole);
+            console.log('Name:', name);
+            console.log('Username:', username);
             console.log('Mobile Number:', mobileNumber);
             console.log('Password:', password);
             console.log('Confirm Password:', confirmPassword);
-
-            
-            const response=await axios.post(REGISTER_URL,
-                JSON.stringify({role,firstName,lastName,email,nicNo,password,role}),
-                {
-                    headers:{'Content-Type':'application/json'},
-                    withCredentials:true
-
-                });
-                console.log(response.data);
-                console.log(response.accessToken);
-                console.log(JSON.stringify(response));
-    //    }
+        }
     };
 
     const goBack = () => {
@@ -144,52 +128,30 @@ const SignupForm = () => {
                     Sign Up
                 </h1>
                 <div className='dropdown'>
-                <select value={role} onChange={(e) => setRole(e.target.value)}>
+                <select value={selectedRole} onChange={(e) => setSelectedRole(e.target.value)}>
                         <option value="" className='role-list' disabled>Select your role</option>
                         {roles.map((role) => (
                         <option key={role} value={role} className='role-list'>{role}</option>
                     ))}
                 </select>
+                
                 </div>
                 <div className='input-box1'>
                     <input 
                         type="text" 
-                        placeholder='First Name'
-                        value={firstName}
-                        onChange={(e) => setFirstName(e.target.value)} 
+                        placeholder='Name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)} 
                         title={inputTitles.name} 
                         required
                     />   
                 </div>
-
                 <div className='input-box1'>
                     <input 
                         type="text" 
-                        placeholder='Last Name'
-                        value={lastName}
-                        onChange={(e) => setLastName(e.target.value)} 
-                        title={inputTitles.name} 
-                        required
-                    />       
-                </div>
-
-                <div className='input-box1'>
-                    <input 
-                        type="text" 
-                        placeholder='Email Address' 
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)} 
-                        title={inputTitles.username} 
-                        required
-                    />
-                </div>            
-
-                <div className='input-box1'>
-                    <input 
-                        type="text" 
-                        placeholder='NIC No' 
-                        value={nicNo}
-                        onChange={(e) => setNicNo(e.target.value)} 
+                        placeholder='Username' 
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)} 
                         title={inputTitles.username} 
                         required
                     />
